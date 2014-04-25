750dfb73006a69db65d36504dee0770f21e83a42
Joe Acosta <joseph.acosta@macys.com>
2014-04-25 11:07:17 -0700

    update docco to create a directory structure for files, so that we can have multiple files with the same name

--- tasks/docco.js
+++ tasks/docco.js
@@ -5,12 +5,42 @@
 // Licensed under the MIT license.
 
 "use strict";
-var docco = require('docco');
+var docco = require( 'docco' ),
+    grunt = require( "grunt" );
 
-module.exports = function(grunt) {
-  grunt.registerMultiTask('docco', 'Docco processor.', function() {
-    // Either set the destination in the files block, or (prefferred) in { options: output }
-    this.options.output = this.options.output || (this.file && this.file.dest);
-    docco.document(this.options({ args: this.filesSrc }), this.async());
-  });
+module.exports = function ( grunt ) {
+    grunt.registerMultiTask( 'docco', 'Docco processor.', function () {
+        var task = this,
+            options, outputDir,
+            fdone = 0,
+            flength = this.files.length,
+            done = this.async();
+
+        options = task.options();
+        outputDir = options.output;
+
+        this.files.forEach( function ( file ) {
+            // join 'args' and 'output' from options or file.dest according to docco.document() signature/first param
+
+            file.src.forEach( function ( name ) {
+                var out, opts, outdir;
+                out = name.substring( name.lastIndexOf( "/" ) );
+
+                outdir = outputDir + "/" + name.replace( out, "" ).replace( /\//g, "." );
+                opts = {
+                    args: [ name ],
+                    output: outdir
+                };
+                //opts.output = outdir;
+                //console.log( opts );
+                //grunt.verbose.writeflags( opts, 'Options' );
+                docco.document( opts, function () {
+                    // done once all the file sets are processed
+                    if ( ++fdone === flength ) {
+                        done();
+                    }
+                } );
+            } );
+        } );
+    } );
 };
